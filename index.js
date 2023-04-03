60683aa0ea79f9308a889465cadb1eafdb2d366b
jubewe <ju.be.we@posteo.de>
2023-04-03 16:53:14 +0200

    v0.2.9

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 require("./lib/endpoints/_validatetoken");
 require("./lib/endpoints/ban");
 require("./lib/endpoints/deleteMessage");
-// require("./lib/endpoints/gedtUsers");
+require("./lib/endpoints/getUsers");
+require("./lib/endpoints/_getUsers");
 require("./lib/endpoints/shoutout");
 require("./lib/endpoints/timeout");
 require("./lib/endpoints/unban");
 require("./lib/endpoints/whisper");
-
 require("./lib/endpoints/announce");
+require("./lib/endpoints/updateChatSettings");
+
 require("./lib/endpoints/getChatSettings");
 require("./lib/endpoints/getStreams");
+require("./lib/endpoints/mod");
+require("./lib/endpoints/unmod");
+require("./lib/endpoints/vip");
+require("./lib/endpoints/unvip");
+require("./lib/endpoints/updateColor");
+require("./lib/endpoints/getColor");
+require("./lib/endpoints/raid");
+require("./lib/endpoints/getChannelFollowers");
 
 require("./lib/arguments/chatSettings");
 require("./lib/arguments/oberknechtAPIOptions");
